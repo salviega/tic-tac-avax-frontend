a872488afc5b0f8b28fbf3042d8a47424568ee29
Leonardo Ramirez <juanlov4321@gmail.com>
2024-08-31 18:25:35 -0500

    🚩 Added new style and order of elements

--- src/components/shared/Footer/index.tsx
+++ src/components/shared/Footer/index.tsx
@@ -1,64 +1,9 @@
-import { Link } from 'react-router-dom'
-
 export default function Footer() {
 	return (
-		<footer className='flex flex-col items-center py-3 px-8 z-10 border-t-2 text-sm font-light'>
-			<ul className='flex flex-col items-end w-[1100px] gap-4'>
-				<div className='flex flex-wrap items-center gap-4'>
-					<li>
-						<Link to='/request-demo'>Request Demo |</Link>
-					</li>
-					<li>
-						<Link to='/about'>About |</Link>
-					</li>
-					<li>
-						<Link to='/terms'>Terms |</Link>
-					</li>
-					<li>
-						<Link to='/privacy'>Privacy |</Link>
-					</li>
-					<li>
-						<Link to='/developers'>Developers |</Link>
-					</li>
-					<li>
-						<Link to='/support'>Support |</Link>
-					</li>
-				</div>
-				<div className='flex flex-wrap items-center gap-4'>
-					<li>
-						<Link to='/cookies'>Cookies |</Link>
-					</li>
-					<li>
-						<Link to='/do-not-sell-my-info'>
-							Do Not Sell My Personal Information
-						</Link>
-					</li>
-				</div>
-				<div className='flex flex-wrap items-center gap-4'>
-					<a
-						href='https://www.apple.com/app-store/'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						<img
-							className='rounded-full size-20'
-							src='https://pbs.twimg.com/profile_images/1788729114792263680/DAJoQA6p_400x400.jpg'
-							alt='logo'
-						/>
-					</a>
-					<a
-						href='https://www.apple.com/app-store/'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						<img
-							className='rounded-full size-20'
-							src='https://pbs.twimg.com/profile_images/1788729114792263680/DAJoQA6p_400x400.jpg'
-							alt='logo'
-						/>
-					</a>
-				</div>
-			</ul>
+		<footer className="flex flex-col items-center py-5 px-8 z-10 text-sm font-light bg-[#37005B]/50 text-white">
+			<p className="text-xs mt-4 sm:mt-0">
+				&copy; {new Date().getFullYear()} Tic Tac Toe Avax.	 All rights reserved.
+			</p>
 		</footer>
-	)
+	);
 }
--- src/components/shared/Navbar/index.tsx
+++ src/components/shared/Navbar/index.tsx
@@ -2,12 +2,19 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 export default function Navbar(): JSX.Element {
 	return (
-		<nav className='flex flex-col items-center w-full h-32 py-5 px-8 z-10 border-b-2 box-border shadow-md text-sm font-light'>
-			<ul className='flex justify-around items-center w-full '>
-				<li>
+		<nav
+			className="top-0 sticky flex flex-col items-center w-full h-32 py-5 px-8 z-10 shadow-md text-sm font-light backdrop-blur bg-[#37005B]/50 box-border md:flex-row md:justify-between md:h-20"
+			aria-label="Main Navigation"
+		>
+			<div className="flex items-center justify-center w-full md:w-auto">
+				<img src="src/assets/images/TicTacToe-Avax.png" alt="Logo TicTacToe" className='w-[200px]' />
+			</div>
+			<ul className="flex flex-col justify-around items-center w-full md:flex-row md:w-auto space-y-4 md:space-y-0 md:space-x-8" role="menu">
+				<li role="menuitem">
 					<ConnectButton />
 				</li>
 			</ul>
 		</nav>
+
 	)
 }
