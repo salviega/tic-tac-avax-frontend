e4a88a895f31520f1b8c63f84d002d0f957ec134
Leonardo Ramirez <juanlov4321@gmail.com>
2024-08-31 18:27:08 -0500

    🚩 Loading and logic of componentes to the star game tic tac toe

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,51 @@
+import { useEffect, useState } from 'react';
+import { useAccount } from 'wagmi';
+
+// import BackgroundAudio from '@/components/BackGroundSound';
+import FormPlayers from '@/components/FormPlayers';
+import NotAccount from '@/components/shared/NotAccount';
+import CyberpunkBentoTicTacToe from '@/components/TicTacToe';
+
+import Loading from '../../components/shared/Loading/index';
+
 export default function Home(): JSX.Element {
-	return <div className='flex flex-col items-center'></div>
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [isStartGame, setIsStartGame] = useState<boolean>(false);
+	const { isConnected } = useAccount();
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setIsLoading(false);
+		}, 5000);
+
+		return () => clearTimeout(timer);
+	}, []);
+
+	const startGame = () => {
+		setIsStartGame(true);
+	}
+
+	return (
+		<div className='flex justify-center items-center flex-col min-h-lvh'>
+			{isLoading ? (
+				<Loading />
+			) : (
+				<>
+					{
+						isConnected ? <div className="">
+
+							{!isStartGame ? <FormPlayers startGame={startGame} /> : <CyberpunkBentoTicTacToe />}
+
+						</div>
+							:
+
+							<NotAccount />
+					}
+					{/* <BackgroundAudio audioSrc='src/assets/sounds/menuSound.mp3' /> */}
+					{/* <h1 className='text-white font-bold'>CyberpunkBentoTicTacToe</h1> */}
+
+					{/* <CyberpunkBentoTicTacToe /> */}
+				</>
+			)}
+		</div>
+	);
 }
